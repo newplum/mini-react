83f5cd9b24517face69b7f4029680f86f6132c9b
newplum <1105372736@qq.com>
2024-03-24 23:49:02 +0800

    fiber

--- vite-runner/core/react.js
+++ vite-runner/core/react.js
@@ -20,28 +20,88 @@ function createTextNode(text) {
   };
 }
 
+let nextWorkOfUnit = null;
 function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+}
 
-  const props = el.props;
+function workLoop(deadline) {
+  let timeRemainingGt1 = true;
+  while (timeRemainingGt1 && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit);
+    timeRemainingGt1 = deadline.timeRemaining > 1;
+  }
 
+  requestIdleCallback(workLoop);
+}
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updateProps(dom, props) {
   for (const key in props) {
     if (key !== "children") {
       dom[key] = props[key];
     }
   }
+}
 
-  const children = el.props.children;
+function initChildLink(work) {
+  const children = work.props.children;
+  let preChild = null;
+  children.forEach((child, index) => {
+    const newWork = {
+      type: child.type,
+      props: child.props,
+      parent: work,
+      child: null,
+      sibling: null,
+      dom: null,
+    };
+    if (index === 0) {
+      work.child = newWork;
+    } else {
+      preChild.sibling = newWork;
+    }
+    preChild = newWork;
+  });
+}
 
-  for (const child of children) {
-    render(child, dom);
+function performWorkOfUnit(work) {
+  if (!work.dom) {
+    // 创建dom
+    const dom = (work.dom = createDom(work.type));
+
+    work.parent.dom.append = dom;
+
+    // 更新props
+    updateProps(dom, work.props);
+  }
+  // 实现链表
+  initChildLink(work);
+
+  // 返回下一个节点
+
+  if (work.child) {
+    return work.child;
   }
 
-  container.append(dom);
+  if (work.sibling) {
+    return work.sibling;
+  }
+
+  return work.parent?.sibling;
 }
+
+requestIdleCallback(workLoop);
 
 export default {
   render,
